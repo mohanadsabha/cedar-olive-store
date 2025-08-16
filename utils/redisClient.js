3620d1ac8191fb0d952c53cf225f1f684deda7f5
Mohanad Abusabha <eng.mohanadsabha@gmail.com>
2025-08-16 15:13:11 +0300

    Added Timeout tries for Redis Client connection

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -6,6 +6,11 @@ const client = redis.createClient({
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
+        connectTimeout: 10000,
+        reconnectStrategy: (retries) => {
+            if (retries > 10) return new Error('Too many retries');
+            return Math.min(retries * 100, 3000);
+        },
     },
 });
 
